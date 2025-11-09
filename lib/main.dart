500e107dbeed18a34b2284f1f59178a355c77f9b
QaimRazaa <qaimr320@gmail.com>
2025-11-09 21:42:43 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
